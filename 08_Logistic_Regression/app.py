ac2e554449802841f2c8dcf7ba1456dcffa34fc2
Gopal-Behere <gopalbehere13@gmail.com>
2026-02-17 00:18:31 +0530

    Add files via upload

---
+++ 08_Logistic_Regression/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load("model.pkl")
+
+st.title("Diabetes Prediction App")
+
+preg = st.number_input("Pregnancies")
+glucose = st.number_input("Glucose")
+bp = st.number_input("Blood Pressure")
+skin = st.number_input("Skin Thickness")
+insulin = st.number_input("Insulin")
+bmi = st.number_input("BMI")
+dpf = st.number_input("Diabetes Pedigree Function")
+age = st.number_input("Age")
+
+if st.button("Predict"):
+    data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
+    result = model.predict(data)
+
+    if result[0] == 1:
+        st.error("Patient has Diabetes")
+    else:
+        st.success("Patient does not have Diabetes")
